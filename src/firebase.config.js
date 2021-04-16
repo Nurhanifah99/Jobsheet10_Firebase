d3d3ef3a3e55ae3326d393cc69bcb2cdfeefffad
Nurhanifah99 <Hanifah110498@gmail.com>
2021-04-16 17:54:41 +0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBJ1TaBE2s8A13lXGvXGQSS-vrWItB3bfw",
+    authDomain: "modul10-321c6.firebaseapp.com",
+    projectId: "modul10-321c6",
+    storageBucket: "modul10-321c6.appspot.com",
+    messagingSenderId: "65977240972",
+    appId: "1:65977240972:web:f8b326b5de311116635aa9",
+    measurementId: "G-4KZXLVDG1Z"
+};
